ceab2d42da12e5aca943684eca5b12b8d21f8b04
pankovakris <pankovakrisss@gmail.com>
2022-03-12 17:40:20 +0300

    -

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -3,6 +3,6 @@
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
     "client_id": "8080922",
-    "access_token": "eeb11dd9ce6b5ac695b75e2038ba8d4d60912f1f17edb09ab5498e9e0d137720e5a9604e0c3adce6fc3e4",
+    "token": "eeb11dd9ce6b5ac695b75e2038ba8d4d60912f1f17edb09ab5498e9e0d137720e5a9604e0c3adce6fc3e4",
     "version": "5.126",
 }
