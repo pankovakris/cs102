d14173ee4e4cacf3c157a32a0f31810ab8bbd184
pankovakris <pankovakrisss@gmail.com>
2022-05-10 14:11:27 +0300

    lab 3

--- homework06/db.py
+++ homework06/db.py
@@ -1,17 +1,17 @@
-from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from scraputils import get_news
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
-
+Base.metadata.create_all(bind=engine)
 
 class News(Base):
     __tablename__ = "news"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key = True)
     title = Column(String)
     author = Column(String)
     url = Column(String)
@@ -19,4 +19,16 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
-Base.metadata.create_all(bind=engine)
+s = session()
+
+# news = get_news("https://news.ycombinator.com/newest", 35)
+# for new in news:
+#     elem = News(
+#         title = new['title'],
+#         author=new['author'],
+#         url = new['url'],
+#         comments = new['comments'],
+#         points = new['points'],
+#     )
+#     s.add(elem)
+# s.commit()
--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,34 +1,62 @@
-from bottle import (
-    route, run, template, request, redirect
-)
-
-from scrapper import get_news
+from bottle import route, run, template
 from db import News, session
+from bottle import redirect, request
+from scraputils import get_news
 from bayes import NaiveBayesClassifier
 
 
-@route("/news")
+@route('/')
+@route('/news')
 def news_list():
     s = session()
     rows = s.query(News).filter(News.label == None).all()
     return template('news_template', rows=rows)
 
 
-@route("/add_label/")
+@route('/add_label/')
 def add_label():
-    # PUT YOUR CODE HERE
-    redirect("/news")
+    s = session()
+    lb = request.query.label
+    cur_id = request.query.id
+    post = s.query(News).filter(News.id == cur_id).all()[0]
+    post.label = lb
+    s.commit()
+    redirect('/news')
 
 
-@route("/update")
+@route('/update_news')
 def update_news():
-    # PUT YOUR CODE HERE
-    redirect("/news")
+    s = session()
+    news = get_news("https://news.ycombinator.com/newest", 3)
+    for new in news:
+        if len(s.query(News).filter(News.author == new['author'] and News.title == new['title']).all()) == 0:
+            elem = News(
+            title = new['title'],
+            author=new['author'],
+            url = new['url'],
+            comments = new['comments'],
+            points = new['points'],
+            )
+            s.add(elem)
+    s.commit()
+    redirect('/news')
 
-
-@route("/classify")
-def classify_news():
-    # PUT YOUR CODE HERE
+@route('/recommendations')
+def recommendations():
+    s = session()
+    empty = s.query(News).filter(News.label == None).all()
+    simp_rows = [x.title for x in empty ]
+    class_rows = [x.title for x in s.query(News).filter(News.label != None).all() ]
+    labels = [news.label for news in s.query(News).filter(News.label != None).all()]
+    model = NaiveBayesClassifier()
+    model.fit(class_rows, labels)
+    predicts = model.predict(simp_rows)
+    for x in range(len(predicts)):
+        empty[x].label = predicts[x]
+    classified_news = [x  for x in empty if x.label == 'good']
+    classified_news.extend( [x  for x in empty if x.label == 'maybe'] )
+    classified_news.extend( [x  for x in empty if x.label == 'never'] )
+    return template('recs.tpl', rows=classified_news[1:])
 
 
 if __name__ == "__main__":
